bccc977c888b791447bfb87539be524e30c0e920
samuel <samuelpc4567@gmail.com>
2026-03-16 09:24:32 -0300

    Adicionando icone na tela de gerenciar partida e na de gerenciar equipes

--- front-end/src/views/quadra_play/GerenciarEquipesCampeonatoView.vue
+++ front-end/src/views/quadra_play/GerenciarEquipesCampeonatoView.vue
@@ -92,7 +92,10 @@
               class="btn-jogadores btn-jogadores-principal btn-jogadores-bloco"
               @click.stop="abrirModalJogadoresEquipe(equipe)"
             >
-              Jogadores
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="btn-jogadores-icon" viewBox="0 0 16 16" aria-hidden="true">
+                <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+              </svg>
+              <span>Jogadores</span>
             </button>
           </article>
         </div>
@@ -947,6 +950,14 @@ export default {
   font-size: 13px;
   font-weight: 700;
   cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+}
+
+.btn-jogadores-icon {
+  flex: 0 0 auto;
 }
 
 .btn-jogadores:hover {
--- front-end/src/views/quadra_play/GerenciarPartidaView.vue
+++ front-end/src/views/quadra_play/GerenciarPartidaView.vue
@@ -173,13 +173,42 @@
                 :disabled="(isMesario && partida.status === 'FINALIZADA') || partidaAcessandoId === partida.id"
                 @click="acessarPartida(partida)"
               >
-                {{
-                  partidaAcessandoId === partida.id
-                    ? 'Acessando...'
-                    : partida.status === 'FINALIZADA'
-                      ? (isMesario ? 'Finalizada' : 'Editar')
-                      : 'Acessar'
-                }}
+                <span class="btn-acessar-content">
+                  <svg
+                    v-if="partidaAcessandoId !== partida.id && partida.status !== 'FINALIZADA'"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="btn-acessar-icon"
+                    viewBox="0 0 16 16"
+                    aria-hidden="true"
+                  >
+                    <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+                    <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                  </svg>
+                  <svg
+                    v-else-if="partidaAcessandoId !== partida.id && partida.status === 'FINALIZADA' && !isMesario"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="btn-acessar-icon"
+                    viewBox="0 0 16 16"
+                    aria-hidden="true"
+                  >
+                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+                  </svg>
+                  <span>
+                    {{
+                      partidaAcessandoId === partida.id
+                        ? 'Acessando...'
+                        : partida.status === 'FINALIZADA'
+                          ? (isMesario ? 'Finalizada' : 'Editar')
+                          : 'Acessar'
+                    }}
+                  </span>
+                </span>
               </button>
               <button v-else class="btn-acessar" @click="removerPartidaCancelada(partida)">
                 Remover
@@ -1827,6 +1856,17 @@ a {
   letter-spacing: 0.08em;
   text-transform: uppercase;
   box-shadow: 0 10px 18px rgba(59, 130, 246, 0.18);
+}
+
+.btn-acessar-content {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+}
+
+.btn-acessar-icon {
+  flex: 0 0 auto;
 }
 
 .btn-acessar:hover {
