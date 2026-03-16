5c0ca1ddaa4d9ad61098f012041d7526d5cc79b9
samuel <samuelpc4567@gmail.com>
2026-03-16 08:56:23 -0300

    Adicionando icones nos botões da tela de usuarios

--- front-end/src/views/UsuariosView.vue
+++ front-end/src/views/UsuariosView.vue
@@ -109,12 +109,25 @@
 
               <div class="botoes">
                 <button class="btn-editar" @click="editarUsuario(usuario)">
-                  {{
-                    permissaoLogadoId === 2 ? 'Gerenciar usuário' : 'Alterar permissões'
-                  }}
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="btn-action-icon" viewBox="0 0 16 16" aria-hidden="true">
+                    <path
+                      d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m.256 7a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1zm3.63-4.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0" />
+                  </svg>
+                  <span class="btn-action-label">
+                    {{ permissaoLogadoId === 2 ? 'Gerenciar usuário' : 'Alterar permissões' }}
+                  </span>
                 </button>
 
-                <button class="btn-detalhar" @click="detalhesUsuario(usuario)">Detalhar</button>
+                <button class="btn-detalhar" @click="detalhesUsuario(usuario)">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="btn-action-icon" viewBox="0 0 16 16" aria-hidden="true">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                    <path
+                      d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+                  </svg>
+                  <span class="btn-action-label">Detalhar</span>
+                </button>
               </div>
             </div>
           </article>
@@ -1298,7 +1311,17 @@ select:focus {
   display: inline-flex;
   align-items: center;
   justify-content: center;
+   gap: 8px;
   padding: 0 12px;
+  white-space: nowrap;
+}
+
+.btn-action-icon {
+  flex-shrink: 0;
+}
+
+.btn-action-label {
+  min-width: 0;
 }
 
 .btn-editar,
