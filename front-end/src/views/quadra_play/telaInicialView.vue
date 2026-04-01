02d93864530a3e063aa46e86c3d700b2385e49f3
samuel <samuelpc4567@gmail.com>
2026-04-01 08:30:00 -0300

    Alterações na tela inicial de campeonatos

--- front-end/src/views/quadra_play/telaInicialView.vue
+++ front-end/src/views/quadra_play/telaInicialView.vue
@@ -127,25 +127,32 @@
                 @click.stop="abrirCampeonato(campeonato)"
               >
                 <span class="btn-acessar-content">
+                  <span class="btn-acessar-main">
+                    <span
+                      v-if="campeonatoAcessandoId === Number(campeonato.id)"
+                      class="btn-acessar-spinner"
+                      aria-hidden="true"
+                    ></span>
+                    <svg
+                      v-else
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="btn-acessar-icon"
+                      viewBox="0 0 16 16"
+                      aria-hidden="true"
+                    >
+                      <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+                      <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                    </svg>
+                    <span>{{ campeonatoAcessandoId === Number(campeonato.id) ? 'Abrindo...' : 'Acessar' }}</span>
+                  </span>
                   <span
-                    v-if="campeonatoAcessandoId === Number(campeonato.id)"
-                    class="btn-acessar-spinner"
+                    v-if="campeonatoAcessandoId !== Number(campeonato.id)"
+                    class="btn-acessar-arrow"
                     aria-hidden="true"
-                  ></span>
-                  <svg
-                    v-else
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="btn-acessar-icon"
-                    viewBox="0 0 16 16"
-                    aria-hidden="true"
-                  >
-                    <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
-                    <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
-                  </svg>
-                  <span>{{ campeonatoAcessandoId === Number(campeonato.id) ? 'Abrindo...' : 'Acessar' }}</span>
+                  >&gt;</span>
                 </span>
               </button>
             </div>
@@ -756,12 +763,19 @@ a {
 }
 
 .btn-acessar-content {
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  align-items: center;
+  width: 100%;
+  line-height: 1;
+}
+
+.btn-acessar-main {
+  grid-column: 2;
   display: inline-flex;
   align-items: center;
   justify-content: center;
   gap: 8px;
-  width: 100%;
-  line-height: 1;
 }
 
 .btn-acessar-spinner {
@@ -777,6 +791,14 @@ a {
 .btn-acessar-icon {
   flex: 0 0 auto;
   display: block;
+}
+
+.btn-acessar-arrow {
+  justify-self: end;
+  font-size: 18px;
+  font-weight: 900;
+  line-height: 1;
+  opacity: 0.95;
 }
 
 .btn-acessar:hover:not(:disabled) {
